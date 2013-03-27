df05ba0491b56425524ce1befa7427565ef87ac3
simon <simon@BABOUCHE.(none)>
2013-03-27 18:19:39 +0100

    Ajout des fichiers .c et .h + MAJ Makefile (verifier histoire de l'include du .h dans main) + ajout Structure Block

---
+++ include/structures.h
@@ -0,0 +1,6 @@
+
+
+typedef struct{
+	unsigned char value;// 0 if not found yet
+	unsigned char* possibleValues;//	
+}block;
--- src/main.c
+++ src/main.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include "../include/structures.h"
 
 int main (int argc, char **argv)
 {
-	printf("Hello World\n");
+	block** grid; //déclaration du double pointeur de la matrice représentant la grille de jeu
+	
+	
 	exit(EXIT_SUCCESS);
 }
